9680b1e1bff9d56ff6eafffd11da9e97f5c1df28
Aubrey Manson <aubreymanson1987@gmail.com>
2019-01-17 19:14:14 -0800

    button==> btn-outline-success

--- form.js
+++ form.js
@@ -16,4 +16,40 @@ $("sendEmail").click(function() {
 
 // Clear all fields in form
 
+// Scrollspy added to tabs and Body of page
 $("body").scrollspy({ target: "#navbar-scroll" });
+
+// Scroll Reveal
+
+(function scrollReveal() {
+  window.sr = ScrollReveal();
+
+  sr.reveal(
+    ".card",
+    {
+      duration: 600,
+      distance: "20px",
+      easing: "ease-out",
+      origin: "bottom",
+      reset: true,
+      scale: 1,
+      viewFactor: 0,
+      afterReveal: revealChildren
+    },
+    150
+  );
+
+  var revealChildren = sr.reveal(
+    ".card-title, .card-text",
+    {
+      duration: 500,
+      scale: 1,
+      distance: "20px",
+      origin: "bottom",
+      reset: true,
+      easing: "ease-out",
+      viewFactor: 1
+    },
+    75
+  );
+})();
